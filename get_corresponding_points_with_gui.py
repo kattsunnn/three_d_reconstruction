6c60e069090e166ac12fc10b746e60f49c64a471
naoki <katsudanaoki@gmail.com>
2026-02-11 16:26:32 +0900

    feat:対応点を描画した全方位画像を保存する機能を追加

--- get_corresponding_points_with_gui.py
+++ get_corresponding_points_with_gui.py
@@ -1,18 +1,21 @@
 import sys
 import json
-from img_utils.img_utils import load_imgs, get_img_points_with_gui
+import img_utils.img_utils as iu 
 
-img_path = sys.argv[1]
+img_dir_path = sys.argv[1]
 window_scale = float(sys.argv[2])
 output_file_path = sys.argv[3]
+output_img_dir = sys.argv[4]
 
-imgs = load_imgs(img_path)
+imgs = iu.load_imgs(img_dir_path)
 
 save_data = []
+drawn_imgs = []
 for img in imgs:
-    corr_points, _ = get_img_points_with_gui(img, window_scale)
+    corr_points, drawn_img = iu.get_img_points_with_gui(img, window_scale)
     save_data.append(corr_points.tolist())
-
+    drawn_imgs.append(drawn_img)
 with open(output_file_path, 'w', encoding='utf-8') as f:
     json.dump(save_data, f, indent=4)
 
+iu.save_imgs(drawn_imgs, output_img_dir, file_name_pattern=f"camera{{}}_corr_points")
